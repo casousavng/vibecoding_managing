5159d268ab319f5000825311a2b45223eaa2c62e
Carlos Sousa <ispg2022105675@ispgaya.pt>
2025-12-02 00:30:21 +0000

    update index to fix permitions

--- server/index.ts
+++ server/index.ts
@@ -33,7 +33,8 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production",
+      //secure: process.env.NODE_ENV === "production",
+      secure: false, // Set to false for development; change to true in production with HTTPS
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     },
     store: new MemoryStore({
